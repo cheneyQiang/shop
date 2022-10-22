f10983682f7edb7151c51f5632993f14b1e81afa
echoQiang <chenzhiqiang0730@163.com>
2022-10-22 22:36:26 +0800

    test

--- index.java
+++ index.java
@@ -1,5 +1,15 @@
 public class index{
-  public static void main(){
-    System.out.println("index");
+
+  public static void main(String[] args) {
+    int s = 10;
+    System.out.println("----");
+    fun(s);
+    System.out.println("----");
+    System.out.println(s);
   }
+  public static void fun(int a) {
+//    a = 20;
+    System.out.println(a);
+  }
+
 }
